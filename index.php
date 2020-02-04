c6f547881166ca375e18819b4bb1ceb3f585d448
stefan <stefan.s.webdev@gmail.com>
2020-02-04 10:10:30 +0100

    Practice

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php 
 
 $myArray = array(
-    array( 1, 2, 3 ),
-    array( 4, 3, 5 ),
-    array( 1, 2, 2 )
+    array( 1, 2, 3, 8 ),
+    array( 4, 3, 5, 7 ),
+    array( 1, 2, 2, 6 )
   );
 
 $length = count($myArray);
-$i = 0;
-for ($i=0; $i < $length - 1 ; $i++) { 
-    for ($j=0; $j < $myArray[i]; $j++) { 
-        echo $myArray[i][j];
+
+for ($i=0; $i < $length; $i++) { 
+    for ($j=0; $j < count($myArray[$i]); $j++) { 
+        echo $myArray[$i][$j] . " " ;
     }
+    echo '<br><br>';
 }
 
