89558e9a4739f4c08bcb7a10f57640078b881aca
stefan <stefan.s.webdev@gmail.com>
2020-02-05 21:53:39 +0100

    Shift array to left

--- index.php
+++ index.php
@@ -3,13 +3,33 @@
 
 
     
-$array = array(1, 2, 3, 4);
+$array = array(1, 2, 3, 4, 5);
 $length = count($array);
-for ($i=0; $i < $length ; $i++) { 
-    for ($j= $length; $j > $length ; $j--) { 
-        $array[$i] = $array[$j];
-    }
+$back = $length - 1;
+
+$n = 1;
+for ($i = 0; $i < count($array); $i++) {   
+    print($array[$i] . " ");   
+}  
+
+for ($i = 0; $i < $n; $i++){  
+    //Stores the first element of the array  
+    $first = $array[0];  
+      
+    for($j = 0; $j < count($array)-1; $j++){  
+        //Shift element of array by one  
+        $array[$j] = $array[$j+1];  
+    }  
+    //First element of array will be added to the end  
+    $array[$j] = $first;  
 }
-print_r($array);
+print("<br>");  
+   
+//Displays resulting array after rotation  
+print("Array after left rotation: <br>");  
+for ($i = 0; $i < count($array); $i++) {   
+    print($array[$i] . " ");   
+}        
+
 
 
